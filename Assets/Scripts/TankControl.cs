d3d7c71cbb72617033296a1a404de075ccb7a1c8
aurisimo <lape_aurelijus@yahoo.com>
2018-08-08 23:33:25 +0300

    #PlayerEnvCreation
    -Player enhanced
    -Background added

--- Assets/Scripts/TankControl.cs
+++ Assets/Scripts/TankControl.cs
@@ -2,16 +2,28 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Control : MonoBehaviour {
+public class TankControl : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-		
-	}
+    private Rigidbody2D _rigidbody;
+    private float _speed = 1f;
+
+    // Use this for initialization
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _rigidbody.rotation += _speed;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _rigidbody.rotation -= _speed;
+        }
+    }
 }
